bc4f5ffd8bd9efa1d5151640a701b77d1cec4ee3
Vladyslav Krot <vladyslav.krot@gmail.com>
2026-05-19 15:01:11 +0300

    chore: code quality

--- src/indicatorHelpers.ts
+++ src/indicatorHelpers.ts
@@ -16,8 +16,12 @@ export type IndicatorSize = {
 };
 
 export const toIndicatorSize = (indicatorStyle: IndicatorShape, size: string, lineHeight: number): IndicatorSize => {
-    if (indicatorStyle === IndicatorShape.Circle) return { height: size, width: size, radius: '50%' };
-    if (indicatorStyle === IndicatorShape.Square) return { height: size, width: size, radius: '10%' };
+    if (indicatorStyle === IndicatorShape.Circle) {
+        return { height: size, width: size, radius: '50%' };
+    }
+    if (indicatorStyle === IndicatorShape.Square) {
+        return { height: size, width: size, radius: '10%' };
+    }
     return { height: size, width: lineHeight <= 10 ? toPixels(lineHeight.toString()) : '10px', radius: '0%' };
 };
 
@@ -36,10 +40,16 @@ export const getIndicatorOffset = (
     }
     // Bar shape
     if (lineHeight <= 2) {
-        if (!isEditing) return 1.2;
+        if (!isEditing) {
+            return 1.2;
+        }
         return !row.left || !row.right ? 1.6 : 1.8;
     }
-    if (lineHeight === 3) return 1.2;
-    if (lineHeight >= 4 && lineHeight < 8) return 1;
+    if (lineHeight === 3) {
+        return 1.2;
+    }
+    if (lineHeight >= 4 && lineHeight < 8) {
+        return 1;
+    }
     return 0;
 };
